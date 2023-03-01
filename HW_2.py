2f631ed5e998b75ddec953d2972c2d7c136bdc33
Alekseev Oleg <qwertyray1995@gmail.com>
2023-03-01 22:41:13 +0300

    Добавьте файлы проекта.

---
+++ HW_2.py
@@ -0,0 +1,41 @@
+
+# Задача 10
+
+# from random import randint
+
+# n = int[input('enter number of coins')]
+
+# a = [random.randint(0, 1) for i in range(n)]
+# print(a)
+# sum_0 = 0
+# sum_1 = 0
+
+# for i in range(n)
+#    if a[i] == 0: 
+#       sum_0 += 1
+#    else: 
+#        sum_1 += 1
+# if sum_0 < sum_1: 
+#   print(sum_0)
+
+# else: 
+#    print(sum_1)
+#
+ 
+# Задача 12
+
+# x = int(input())
+# y = int(input())
+# for a in range(x):
+# for b in range(y):
+# if x == a + b and y == a * b:
+# print(a, b)
+
+
+# Задача 14
+
+# n = int(input())
+# i = 0
+# while 2 ** i <= n:
+# print(2 ** i)
+# i += 1
