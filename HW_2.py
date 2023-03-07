27478063e163269bf7d854f936ee144b4493df55
olegastra-git <112951133+olegastra-git@users.noreply.github.com>
2023-03-07 22:45:06 +0300

    Update HW_2.py

--- HW_2.py
+++ HW_2.py
@@ -15,21 +15,21 @@
  
 # Задача 18
 
-import random
+#import random
 
-N = int(input("Введите количество элементов в массиве "))
-list = [random.randint(1, 20) for i in range(N)]
-print(list)
-x = int(input("Введите искомое число "))
-index_element = 0
-min_element = abs(x - list[0])
-for i in range(1, N):
-    buffer_element = abs(x -list[i])
-    if buffer_element < min_element:
-        min_element = buffer_element
-        index_element = i
+# N = int(input("Введите количество элементов в массиве "))
+# list = [random.randint(1, 20) for i in range(N)]
+# print(list)
+# x = int(input("Введите искомое число "))
+# index_element = 0
+# min_element = abs(x - list[0])
+# for i in range(1, N):
+#     buffer_element = abs(x -list[i])
+#    if buffer_element < min_element:
+#        min_element = buffer_element
+#        index_element = i
 
-print(f"Самый близкий по величине элемент к заданному числу {list[index_element]}")
+# print(f"Самый близкий по величине элемент к заданному числу {list[index_element]}")
 
 # Задача 20
 
